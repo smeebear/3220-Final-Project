6c8ed96aea50863c6c026c470350a18ddff1bfb1
chasely goodman <cbgoodman44@students.tntech.edu>
2019-04-18 14:16:21 -0500

    plot a basic decision tree for the numeric votes csv file

--- server.R
+++ server.R
@@ -1,123 +1,31 @@
 library(class)
 library(dplyr)
+library(class)
+library(C50)
+library(printr)
 
-votesInit <- read.csv("house-votes-84-numeric.csv", header=TRUE)
+# read in the data
+votes <- read.csv("C:/CSC_3220/house-votes-84-numeric.csv", header=TRUE)
+votes
+# separate into training and non-training
+n = nrow(votes)
+trainIndex = sample(1:n, size = round(0.6*n), replace=FALSE)
+train = votes[trainIndex ,]
+notTrain = votes[-trainIndex ,]
+# separate the non-training into validation and test
+n = nrow(notTrain)
+validationIndex = sample(1:n, size = round(0.5*n), replace=FALSE)
+validation = notTrain[validationIndex,]
+test = notTrain[-validationIndex,]
 
-votesTable = data.frame(VotesFor = 0, Abstained = 0, VotesAgainst = 0)
+
 
 server <- function(input, output) {
-  output$table = DT::renderDataTable(DT::datatable({
-    votes = votesInit
-    if (input$party == "repub")
-    {
-      votes = votesInit %>% filter(party == "republican")
-    }
-    else if (input$party == "democ")
-    {
-      votes = votesInit %>% filter(party == "democrat")
+  output$distPlot <- renderPlot({
+    
+    if(input$model == "C 5.0"){
+      model <- C5.0(party ~., data=train)
+      plot(model)
     }
     
-    
-    if (input$bill == "b1")
-    {
-      votesTable$VotesFor = sum(votes$b1 == 1)
-      votesTable$Abstained = sum(votes$b1 == 0)
-      votesTable$VotesAgainst = sum(votes$b1 == -1)
-    }
-    else if (input$bill == "b2")
-    {
-      votesTable$VotesFor = sum(votes$b2 == 1)
-      votesTable$Abstained = sum(votes$b2 == 0)
-      votesTable$VotesAgainst = sum(votes$b2 == -1)
-    }
-    else if (input$bill == "b3")
-    {
-      votesTable$VotesFor = sum(votes$b3== 1)
-      votesTable$Abstained = sum(votes$b3 == 0)
-      votesTable$VotesAgainst = sum(votes$b3 == -1)
-    }
-    else if (input$bill == "b4")
-    {
-      votesTable$VotesFor = sum(votes$b4 == 1)
-      votesTable$Abstained = sum(votes$b4 == 0)
-      votesTable$VotesAgainst = sum(votes$b4 == -1)
-    }
-    else if (input$bill == "b5")
-    {
-      votesTable$VotesFor = sum(votes$b5 == 1)
-      votesTable$Abstained = sum(votes$b5 == 0)
-      votesTable$VotesAgainst = sum(votes$b5 == -1)
-    }
-    else if (input$bill == "b6")
-    {
-      votesTable$VotesFor = sum(votes$b6 == 1)
-      votesTable$Abstained = sum(votes$b6 == 0)
-      votesTable$VotesAgainst = sum(votes$b6 == -1)
-    }
-    else if (input$bill == "b7")
-    {
-      votesTable$VotesFor = sum(votes$b7 == 1)
-      votesTable$Abstained = sum(votes$b7 == 0)
-      votesTable$VotesAgainst = sum(votes$b7 == -1)
-    }
-    else if (input$bill == "b8")
-    {
-      votesTable$VotesFor = sum(votes$b8 == 1)
-      votesTable$Abstained = sum(votes$b8 == 0)
-      votesTable$VotesAgainst = sum(votes$b8 == -1)
-    }
-    else if (input$bill == "b9")
-    {
-      votesTable$VotesFor = sum(votes$b9 == 1)
-      votesTable$Abstained = sum(votes$b9 == 0)
-      votesTable$VotesAgainst = sum(votes$b9 == -1)
-    }
-    else if (input$bill == "b10")
-    {
-      votesTable$VotesFor = sum(votes$b10 == 1)
-      votesTable$Abstained = sum(votes$b10 == 0)
-      votesTable$VotesAgainst = sum(votes$b10 == -1)
-    }
-    else if (input$bill == "b11")
-    {
-      votesTable$VotesFor = sum(votes$b11 == 1)
-      votesTable$Abstained = sum(votes$b11 == 0)
-      votesTable$VotesAgainst = sum(votes$b11 == -1)
-    }
-    else if (input$bill == "b12")
-    {
-      votesTable$VotesFor = sum(votes$b12 == 1)
-      votesTable$Abstained = sum(votes$b12 == 0)
-      votesTable$VotesAgainst = sum(votes$b12 == -1)
-    }
-    else if (input$bill == "b13")
-    {
-      votesTable$VotesFor = sum(votes$b13 == 1)
-      votesTable$Abstained = sum(votes$b13 == 0)
-      votesTable$VotesAgainst = sum(votes$b13 == -1)
-    }
-    else if (input$bill == "b14")
-    {
-      votesTable$VotesFor = sum(votes$b14 == 1)
-      votesTable$Abstained = sum(votes$b14 == 0)
-      votesTable$VotesAgainst = sum(votes$b14 == -1)
-    }
-    else if (input$bill == "b15")
-    {
-      votesTable$VotesFor = sum(votes$b15 == 1)
-      votesTable$Abstained = sum(votes$b15 == 0)
-      votesTable$VotesAgainst = sum(votes$b15 == -1)
-    }
-    else if (input$bill == "b16")
-    {
-      votesTable$VotesFor = sum(votes$b16 == 1)
-      votesTable$Abstained = sum(votes$b16 == 0)
-      votesTable$VotesAgainst = sum(votes$b16 == -1)
-    }
-    
-    
-    votesTable
-  }))
-  
-
-}
+})}
--- ui.R
+++ ui.R
@@ -13,20 +13,16 @@ ui <- fluidPage(
     sidebarPanel(
     
       # Input: Slider for the number of bins ----
-      selectInput("bill", label = h3("Bill:"), 
-                  choices = list("Bill 1" = "b1", "Bill 2" = "b2", "Bill 3" = "b3", "Bill 4" = "b4", 
-                                 "Bill 5" = "b5", "Bill 6" = "b6", "Bill 7" = "b7", "Bill 8" = "b8", 
-                                 "Bill 9" = "b9", "Bill 10" = "b10",  "Bill 11" = "b11",  "Bill 12" = "b12", 
-                                 "Bill 13" = "b13", "Bill 14" = "b14", "Bill 15" = "b15", "Bill 16" = "b16"), 
-                  selected = "b1"),
-      selectInput("party", label = h3("Party:"),
-                  choices = list("Both" = "both", "Democrat" = "democ", "Republican" = "repub"),
-                  selected = "both")
+    
+      selectInput("model", label = "Model Type",
+                  choices = list("Logistic Regression", "C 5.0"),
+                  selected = "C 5.0")
 
     # Main panel for displaying outputs ----
     
       ),
-    DT::dataTableOutput("table")
+    plotOutput(outputId = "distPlot")
+    
     )
     # Create a new row for the table.
   )
