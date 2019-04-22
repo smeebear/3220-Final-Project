4eabd2db941ceb83ae5799b3442af845b1d92a47
chasely goodman <cbgoodman44@students.tntech.edu>
2019-04-22 13:39:11 -0500

    fix inputs

--- ui.R
+++ ui.R
@@ -14,9 +14,9 @@ ui <- fluidPage(
     
       # Input: Slider for the number of bins ----
       selectInput("education", label = "Education Type",
-                  choices = list("Higher Education" = 4, "Secondary/ Secondary sepcial" =3, "Incomplete Higher"=1, "lower secondary"=2, "Academic Degree"=5)),
+                  choices = list("Higher Education" = 2, "Secondary/ Secondary sepcial" =3, "Incomplete Higher"=5, "lower secondary"=4, "Academic Degree"=1)),
       selectInput("ctg_ratio", label = "CREDIT_TO_GOODS_RATIO = AMT_CREDIT / AMT_GOODS_PRICE",
-                  choices = list("ratio <= 1.452"= 1.452, "ratio > 1.452" = 2)),
+                  choices = list("ratio <= 1.145"= 1.140, "ratio > 1.145" = 2)),
       selectInput("days_birth", label = "Age",
                   choices = list("Younger than 40" = -14782, "Older than 40" = -14800)),
       textInput("annuity_length", "ANNUITY_LENGTH = AMT_CREDIT / AMT_ANNUITY", value="12.34"),
@@ -25,7 +25,7 @@ ui <- fluidPage(
       selectInput("name_contract_status_mean", label = "status of previous application",
                   choices = list("Approved" = 1.1, "Refused" =1.72 , "Canceled" =1.42,"Unused Offer" = 1.33)),
       selectInput("days_employed", label = "Employment Length",
-                  choices = list("Less than 5 years" = -1784, "Greater than 5 years" = -1785)),
+                  choices = list("greater than 5 years" = -1785, "less than 5 years" = -1750)),
       selectInput("days_credit_mean", label = "How long before current application did client apply for Credit Bureau credit",
                   choices = list("greater than 2.5 years" = -915, "less than 2.5 years" = -914)),
       selectInput("payment_perc_mean", label = "PAYMENT_PERC = AMT_PAYMENT / AMT_INSTALMENT",
