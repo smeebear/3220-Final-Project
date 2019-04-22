35e5f4f689d008f7d51c373cec344bebcaeaac8b
chasely goodman <cbgoodman44@students.tntech.edu>
2019-04-22 11:08:47 -0500

    more changes

--- ui.R
+++ ui.R
@@ -14,14 +14,13 @@ ui <- fluidPage(
     
       # Input: Slider for the number of bins ----
       selectInput("education", label = "Education Type",
-                  choices = list("1", "2")),
-      selectInput("ctg_ratio", label = "Credit to Goods Ratio",
-                  choices = list("1", "2")),
-      selectInput("days_birth", label = "Days Birth",
-                  choices = list("1", "2")),
-      selectInput("annuity_length", label = "Annuity Length",
-                  choices = list("1", "2")),
-      selectInput("name_prod_type_min", label = "Name Product Type Min",
+                  choices = list("Higher Education" = 4, "Secondary/ Secondary sepcial" =3, "Incomplete Higher"=1, "lower secondary"=2, "Academic Degree"=5)),
+      selectInput("ctg_ratio", label = "CREDIT_TO_GOODS_RATIO = AMT_CREDIT / AMT_GOODS_PRICE",
+                  choices = list("ratio <= 1.452"= 1.452, "ratio > 1.452" = 2)),
+      selectInput("days_birth", label = "Age",
+                  choices = list("Younger than 40" = -14782, "Older than 40" = -14800)),
+      numericInput("annuity_length", "ANNUITY_LENGTH = AMT_CREDIT / AMT_ANNUITY", 12, min = 1, max = 25),
+      selectInput("name_prod_type_min", label = "Name of Product Type",
                   choices = list("1", "2")),
       selectInput("name_contract_status_mean", label = "Name Contract Status Mean",
                   choices = list("1", "2")),
@@ -40,7 +39,7 @@ ui <- fluidPage(
     
       ),
     mainPanel (
-      plotOutput(outputId = "distPlot")
+      textOutput(text)
     )
     
     )
